c4be1ad7b8afd7cc1739676207e80413172ed58e
Sun Kim <biotext@users.noreply.github.com>
2018-04-26 11:09:47 -0400

    Delete sbatch_run.sh

--- sbatch_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH --partition=gpu
-#SBATCH --time=120:00:00
-#SBATCH --mem=120g
-#SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:k80:2
-/data/skim01/run_keras.sh python /data/skim01/PMC/pubmed_run_k80.py
