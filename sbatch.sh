798f9779432aa1d9eba0e5cf22b5385462931a58
Sun Kim <biotext@users.noreply.github.com>
2018-04-26 11:11:37 -0400

    Update sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=120g
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:k80:2
-/data/skim01/run_keras.sh python /data/skim01/PMC/pubmed_run_k80.py
+/data/skim01/run_keras.sh python /path/to/code_to_run_keras.py
